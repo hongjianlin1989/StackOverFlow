37c99baadaf12d858538d9c611a9d733f51d9fa6
hongjian lin <hongjianlin@hongjians-MacBook-Pro.local>
2019-10-18 17:23:11 -0400

    add 3rd party framework

---
+++ Podfile
@@ -0,0 +1,23 @@
+platform :ios, '11.0'
+use_frameworks!
+
+workspace 'StackOverFlow'
+
+#target 'StackOverFlow' do
+#  pod 'Alamofire', '4.7'
+#  pod 'AlamofireImage', '3.3'
+#  pod 'PromiseKit', '~> 6.0'
+#
+#
+#end
+
+
+
+abstract_target 'app' do
+    pod 'Alamofire', '4.7'
+    pod 'AlamofireImage', '3.3'
+    pod 'PromiseKit', '~> 6.0'
+
+    target 'StackOverFlow'
+    
+end
---
+++ StackOverFlow/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  StackOverFlow
+//
+//  Created by hongjian lin on 10/18/19.
+//  Copyright © 2019 com.hongjian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
